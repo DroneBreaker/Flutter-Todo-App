ee52478827d2870d523bc26d68f8b10a4afb6e5e
DroneBreaker <brownierz01@gmail.com>
2024-06-12 23:27:29 +0100

    Initialized the app

---
+++ lib/screens/todo.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class TodoPage extends StatefulWidget {
+  const TodoPage({super.key});
+
+  @override
+  State<TodoPage> createState() => _TodoPageState();
+}
+
+class _TodoPageState extends State<TodoPage> {
+  TextEditingController textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            TextField(
+              controller: textController,
+            ),
+
+            // button
+          ],
+        ),
+      ),
+    );
+  }
+}
